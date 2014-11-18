015e0fbf68786b2bdac51d694e28e6bcec6e57da
linhoo <linhu.li2012@hotmail.com>
2014-11-18 22:43:22 +0800

    Using <context:spring-config/> to Enable @Configuration class

--- src/main/java/org/tobby/jms/spring/client/SendByJmsTemplate.java
+++ src/main/java/org/tobby/jms/spring/client/SendByJmsTemplate.java
@@ -7,6 +7,7 @@ import javax.jms.Message;
 import javax.jms.Session;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
@@ -15,6 +16,7 @@ import org.tobby.jms.spring.client.util.Util;
 public class SendByJmsTemplate {
 
 	public static void main(String[] args) {
+		//AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.tobby.jms.spring.client.config");
 		MessageCreator messageCreator = new MessageCreator() {
 			public Message createMessage(Session session) throws JMSException {
 				return session.createTextMessage(Util.formatDate(new Date()) + "--Ping Ping Ping!");
