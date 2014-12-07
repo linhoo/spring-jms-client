a4833b0c6ee7c51d56ca531bf8c4367b0c61df8f
linhoo <linhu.li2012@hotmail.com>
2014-12-07 21:43:55 +0800

    Implement using the Spring Boot support for JMS. Spring provide the Connection factory and the JmsTemplate internally.

--- src/main/java/org/tobby/jms/spring/client/SendByJmsTemplate.java
+++ src/main/java/org/tobby/jms/spring/client/SendByJmsTemplate.java
@@ -2,42 +2,42 @@ package org.tobby.jms.spring.client;
 
 import java.util.Date;
 
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
+import org.tobby.jms.spring.client.config.AppConfig;
 import org.tobby.jms.spring.client.util.Util;
 
 @Component
 public class SendByJmsTemplate {
 
+	@Autowired
 	private JmsTemplate jmsTemplate;
 
+	@Autowired
 	private Queue queue;
 
-	@Autowired
-	private void setConnectionFactory(ConnectionFactory connectionFactory) {
-		jmsTemplate = new JmsTemplate(connectionFactory);
-	}
+//	@Autowired
+//	private void setConnectionFactory(ConnectionFactory connectionFactory) {
+//		jmsTemplate = new JmsTemplate(connectionFactory);
+//	}
 
-	@Autowired
-	private void setQueue(Queue queue) {
-		this.queue = queue;
-	}
+//	@Autowired
+//	private void setQueue(Queue queue) {
+//		this.queue = queue;
+//	}
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = 
-				new AnnotationConfigApplicationContext("org.tobby.jms.spring.client.config");
+//		AnnotationConfigApplicationContext context = 
+//				new AnnotationConfigApplicationContext("org.tobby.jms.spring.client.config");
 //		MessageCreator messageCreator = new MessageCreator() {
 //			public Message createMessage(Session session) throws JMSException {
 //				return session.createTextMessage(Util.formatDate(new Date()) + "--Ping Ping Ping!");
@@ -45,6 +45,7 @@ public class SendByJmsTemplate {
 //		};
 		//ConfigurableApplicationContext context = 
 		//		new ClassPathXmlApplicationContext("applicationContext.xml");
+		ConfigurableApplicationContext context = SpringApplication.run(AppConfig.class, args);
 		SendByJmsTemplate send = (SendByJmsTemplate)context.getBean("sendByJmsTemplate");
 		send.simpleSend(Util.formatDate(new Date()) + "--Ping Ping Ping!");
 		context.close();
--- src/main/java/org/tobby/jms/spring/client/config/AppConfig.java
+++ src/main/java/org/tobby/jms/spring/client/config/AppConfig.java
@@ -1,25 +1,24 @@
 package org.tobby.jms.spring.client.config;
 
-import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
 import org.apache.activemq.command.ActiveMQQueue;
-import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
+@EnableAutoConfiguration
 @ComponentScan("org.tobby.jms.spring.client")
 public class AppConfig {
 
-	@Bean
-	public ConnectionFactory connectionFactory() {
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		connectionFactory.setBrokerURL("tcp://localhost:61616");
-		return connectionFactory;
-	}
+//	@Bean
+//	public ConnectionFactory connectionFactory() {
+//		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+//		connectionFactory.setBrokerURL("tcp://localhost:61616");
+//		return connectionFactory;
+//	}
 	
 //	@Bean
 //	public JmsTemplate jmsTemplate() {
